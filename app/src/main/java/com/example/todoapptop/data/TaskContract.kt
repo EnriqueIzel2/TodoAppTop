87d0db076cb3b7f83882b37f0c05278e679a0f81
Enrique Izel <eleaoizel@gmail.com>
2023-12-07 15:52:50 -0400

    feat: create TaskContract

---
+++ app/src/main/java/com/example/todoapptop/data/TaskContract.kt
@@ -0,0 +1,10 @@
+package com.example.todoapptop.data
+
+class TaskContract {
+
+  object TaskEntry {
+    const val TABLE_NAME = "tasks"
+    const val COLUMN_DESCRIPTION = "description"
+    const val COLUMN_PRIORITY = "priority"
+  }
+}

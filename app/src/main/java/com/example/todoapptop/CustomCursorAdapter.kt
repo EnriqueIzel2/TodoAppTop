7b86c6f2564b9090c1bc712c6a4249ad569109a0
Enrique Izel <eleaoizel@gmail.com>
2023-12-07 15:52:37 -0400

    feat: implementation of onBindViewHolder

--- app/src/main/java/com/example/todoapptop/CustomCursorAdapter.kt
+++ app/src/main/java/com/example/todoapptop/CustomCursorAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.todoapptop
 
+import android.content.Context
 import android.database.Cursor
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapptop.data.TaskContract
 
 class CustomCursorAdapter : RecyclerView.Adapter<CustomCursorAdapter.TaskViewHolder>() {
 
@@ -29,9 +33,26 @@ class CustomCursorAdapter : RecyclerView.Adapter<CustomCursorAdapter.TaskViewHol
     return TaskViewHolder(view)
   }
 
+  override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+    val descriptionIndex = cursor?.getColumnIndex(TaskContract.TaskEntry.COLUMN_DESCRIPTION) ?: 0
+    val priorityIndex = cursor?.getColumnIndex(TaskContract.TaskEntry.COLUMN_PRIORITY) ?: 0
+
+    cursor?.moveToPosition(position)
+
+    val description = cursor?.getString(descriptionIndex)
+    val priority = cursor?.getInt(priorityIndex) ?: 0
+
+    holder.taskDescription.text = description
+    holder.taskPriority.text = priority.toString()
+
+    val gradientDrawable = holder.taskPriority.background as GradientDrawable
+    val backgroundColor = getPriorityBackgroundColor(holder.taskDescription.context, priority)
+    gradientDrawable.setColor(backgroundColor)
+  }
+
   override fun getItemCount(): Int = cursor?.count ?: 0
 
-  fun swapCursor(c: Cursor?): Cursor? {
+  private fun swapCursor(c: Cursor?): Cursor? {
     if (cursor == c) {
       return null
     }
@@ -46,7 +67,13 @@ class CustomCursorAdapter : RecyclerView.Adapter<CustomCursorAdapter.TaskViewHol
     return temp
   }
 
-  override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-    TODO("Not yet implemented")
+  private fun getPriorityBackgroundColor(context: Context, priority: Int): Int {
+    val color = when (priority) {
+      1 -> R.color.red
+      2 -> R.color.orange
+      else -> R.color.yellow
+    }
+
+    return ContextCompat.getColor(context, color)
   }
 }

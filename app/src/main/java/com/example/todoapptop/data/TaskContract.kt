9229bf0e934ed1095b5710bd1790c1d905a74d1a
Enrique Izel <eleaoizel@gmail.com>
2023-12-09 18:43:03 -0400

    feat: add uri

--- app/src/main/java/com/example/todoapptop/data/TaskContract.kt
+++ app/src/main/java/com/example/todoapptop/data/TaskContract.kt
@@ -1,10 +1,20 @@
 package com.example.todoapptop.data
 
+import android.net.Uri
+
 class TaskContract {
+
+  companion object {
+    private const val AUTHORITY = "com.example.todoapptop"
+    val BASE_CONTENT_URI: Uri = Uri.parse("content://$AUTHORITY")
+    private const val PATH_TASKS = "tasks"
+  }
 
   object TaskEntry {
     const val TABLE_NAME = "tasks"
     const val COLUMN_DESCRIPTION = "description"
     const val COLUMN_PRIORITY = "priority"
+
+    val CONTENT_URI: Uri = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build()
   }
 }

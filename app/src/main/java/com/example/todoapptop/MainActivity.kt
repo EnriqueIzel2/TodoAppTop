ba0f026c685cdc803c35995d415a1540592c3b74
Enrique Izel <eleaoizel@gmail.com>
2023-12-09 21:15:51 -0400

    feat: add swipe delete option

--- app/src/main/java/com/example/todoapptop/MainActivity.kt
+++ app/src/main/java/com/example/todoapptop/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.todoapptop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapptop.data.TaskContract
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
@@ -27,5 +29,26 @@ class MainActivity : AppCompatActivity() {
       val intent = Intent(this, AddTaskActivity::class.java)
       startActivity(intent)
     }
+
+    val callback =
+      object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+        override fun onMove(
+          recyclerView: RecyclerView,
+          viewHolder: RecyclerView.ViewHolder,
+          target: RecyclerView.ViewHolder
+        ): Boolean {
+          return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+          val id = viewHolder.itemView.tag as Int
+          val contentUri = TaskContract.TaskEntry.CONTENT_URI
+          val uri = contentUri.buildUpon().appendPath(id.toString()).build()
+
+          contentResolver.delete(uri, null, null)
+        }
+      }
+
+    ItemTouchHelper(callback).attachToRecyclerView(recyclerView)
   }
 }
